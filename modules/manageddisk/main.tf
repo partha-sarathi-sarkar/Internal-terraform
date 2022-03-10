469148b203379f7ab7bb9879a63417bff5e44e1a
partha sarathi sarkar <partha.sarathi.sarkar95@gmail.com>
2022-03-10 14:06:58 +0530

    initial

---
+++ modules/manageddisk/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_managed_disk" "example" {
+  name                 = var.osdiskname
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = var.storage_account_type
+  create_option        = var.create_option
+  disk_size_gb         = "100"
+
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ modules/manageddisk/variables.tf
@@ -0,0 +1,16 @@
+variable "osdiskname" {
+}
+
+variable "location" {
+}
+
+variable "resource_group_name" {
+}
+
+variable "storage_account_type" {
+  default = "Standard_LRS"
+}
+
+variable "create_option" {
+  default = "FromImage"
+}
