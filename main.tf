15d53e0c2994d69da8ebf134fd1c859c4bbfff4e
Partha Sarathi Sarkar <53785505+partha-sarathi-sarkar@users.noreply.github.com>
2022-03-10 15:52:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "azurerm_managed_disk" "example" {
   storage_account_type = var.storage_account_type
   create_option        = var.create_option
   disk_size_gb         = "100"
+  os_type              = "Linux"
 
   tags = {
     environment = var.environment
